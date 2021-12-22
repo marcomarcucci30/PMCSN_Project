8511f4bd625588de6072df40c9f556c7594e7a45
giuseppelasco17 <giuseppe.lasco17@gmail.com>
2021-12-22 19:16:17 +0100

    - Skeleton advanced model in progress

---
+++ advanced_model/skeleton.py
@@ -0,0 +1,364 @@
+from utils.rngs import random, selectStream, plantSeeds
+from utils.rvgs import Exponential, TruncatedNormal
+
+nodes = 4  # n nodi
+arrival_time = 0.0
+arrival_time_morning = 15.0
+arrival_time_afternoon = 15.0
+arrival_time_evening = 15.0
+arrival_time_night = 15.0
+
+seed = 123456789  # TODO: Controlla il seed migliore o forse era il multiplier?
+START = 8.0 * 1440
+STOP = 1 * 1 * 28 * 1440.0  # Minutes
+INFINITY = STOP * 100.0
+p_ticket_queue = 0.8
+TICKET_QUEUE = 1
+# ARCADE1 = 2
+# ARCADE2 = 3
+# ARCADE3 = 4
+p_size = 0.6
+p_premium = 0.36
+
+
+class Time:
+    current = None  # current time
+    next = None  # next (most imminent) event time
+
+
+time = Time()
+
+
+class Track:
+    node = 0.0  # time integrated number in the node
+    queue = 0.0  # time integrated number in the queue
+    service = 0.0  # time integrated number in service
+    index = 0.0  # jobs departed
+    number = 0.0  # jobs in node
+
+    def __init__(self):
+        self.node = 0.0
+        self.queue = 0.0
+        self.service = 0.0
+        self.index = 0.0  # jobs departed
+        self.number = 0.0  # jobs in node
+
+
+class StatusNode:
+    id = None
+    arrival = None  # next arrival time
+    completion = None  # next completion time
+    last = 0.0  # last arrival time
+    more_p_stat = None
+    less_p_stat = None
+
+    def __init__(self, id_node):
+        self.id = id_node
+        self.more_p_stat = Track()  # Track_more_p stats
+        self.less_p_stat = Track()  # Track_less_p stats
+
+
+class SystemTrack:
+    node = 0.0  # time integrated number in the node
+    queue = 0.0  # time integrated number in the queue
+    service = 0.0  # time integrated number in service
+
+    def __init__(self):
+        self.node = 0.0
+        self.queue = 0.0
+        self.service = 0.0
+
+
+class SystemNode:
+    id = None
+    arrival = None  # next arrival time
+    priority_arrival = False
+    priority_completion = False
+    completion = None  # next completion time
+    last = 0.0  # last arrival time
+    index = 0.0  # jobs departed
+    number = 0.0  # jobs in node
+    stat = None
+
+    def __init__(self, id_node):
+        self.id = id_node
+        self.stat = SystemTrack()  # Track stats
+
+
+def set_arrival_time(x):
+    global arrival_time
+    arrival_time = x
+
+
+def get_arrival_time():
+    return arrival_time
+
+
+arr_est = 0
+
+
+def select_node(from_tkt_queue):
+    selectStream(5)  # TODO: scegliere gli streams sequenziali o no?
+    if from_tkt_queue:
+        r = random()
+        for i in range(1, nodes):
+            if r <= i / (nodes - 1):
+                return i + 1
+    # if r <= 1 / (nodes - 1):
+    #     return ARCADE1
+    # elif r <= 2 / (nodes - 1):
+    #     return ARCADE2
+    # else:
+    #     return ARCADE3
+
+    # Caso arrivo dall'esterno
+
+    r = random()
+    if r <= p_ticket_queue:
+        global arr_est
+        arr_est += 1
+        # print(arr_est)
+        return TICKET_QUEUE
+    else:
+        r = random()
+        for i in range(1, nodes):
+            if r <= i / (nodes - 1):
+                return i + 1
+    #  if r <= 1.0 / float(nodes - 1):
+    #      # print(1.0 / float(nodes - 1))
+    #      return ARCADE1
+    #  elif r <= 2.0 / float(nodes - 1):
+    #      # print(2.0 / float(nodes - 1))
+    #      return ARCADE2
+    #  else:
+    #      return ARCADE3
+
+
+def minimum(a, b):
+    if a is None and b is not None:
+        return b
+    elif b is None and a is not None:
+        return a
+    elif a is None and b is None:
+        return None
+    elif a < b:
+        return a
+    else:
+        return b
+
+
+def next_event():
+    time_event = []
+    for i in range(1, len(node_list)):
+        time_event.append(node_list[i].arrival)
+        time_event.append(node_list[i].completion)
+
+    time_event = sorted(time_event, key=lambda x: (x is None, x))
+
+    for i in range(1, len(time_event)):
+        if time_event[0] == node_list[i].arrival or time_event[0] == node_list[i].completion:
+            return i
+
+
+# TODO: Provare l'esponenziale troncata e verificare i tempi di interarrivo
+def get_arrival(y):
+    # ---------------------------------------------
+    # * generate the next arrival time from an Exponential distribution.
+    # * --------------------------------------------
+
+    selectStream(0)
+    return Exponential(y)
+
+
+def get_service(id_node):
+    # --------------------------------------------
+    # * generate the next service time
+    # * --------------------------------------------
+    # */
+    if id_node == TICKET_QUEUE:
+        selectStream(8)
+        r = random()
+        if r <= p_size:  # green pass
+            selectStream(id_node + 10)
+            service = TruncatedNormal(2, 1.5, 1, 3)  # green pass
+            # print("Green pass: ", service)
+            return service
+        else:
+            selectStream(id_node + 15)
+            service = TruncatedNormal(10, 1.5, 8, 12)  # covid test
+            # print("covid test: ", service)
+            return service
+    else:
+        selectStream(id_node + 10)
+        service = TruncatedNormal(15, 3, 3, 25)  # arcade game time
+        # print("arcade game time: ", service)
+        return service
+
+
+def select_queue(node_id):
+    if node_id == TICKET_QUEUE:
+        selectStream(6)
+        r = random()
+        if r <= p_size:  # green pass
+            node_list[node_id].priority_arrival = True  # green pass
+            return
+        else:
+            node_list[node_id].priority_arrival = False  # test
+            return
+    else:
+        selectStream(7)  # TODO:Controllare gli stream!
+        r = random()
+        if r <= p_premium:  # green pass
+            node_list[node_id].priority_arrival = True  # premium ticket
+            return
+        else:
+            node_list[node_id].priority_arrival = False  # standard ticket
+            return
+
+
+if __name__ == '__main__':
+    # settings
+    node_list = []
+    for i in range(nodes + 1):
+        if i == 0:
+            node_list.append(SystemNode(i))
+        else:
+            node_list.append(StatusNode(i))
+    plantSeeds(seed)
+
+    time.current = START
+    arrival = START  # global temp var for getArrival function     [minutes]
+
+    # initialization of the first arrival event
+    set_arrival_time(arrival_time_night)
+    arrival += get_arrival(arrival_time)
+    node = node_list[select_node(False)]  # node in cui schedulare l'arrivo
+    select_queue(node.id)  # discriminazione della coda relativa all'arrivo
+    node.arrival = arrival
+    min_arrival = arrival
+
+    while (min_arrival < STOP) or (node_list[0].number > 0):
+
+        node_to_process = node_list[next_event()]  # node with minimum arrival or completion time
+        time.next = minimum(node_to_process.arrival, node_to_process.completion)
+        # Aggiornamento delle aree basate sul giro prima
+        for i in range(0, len(node_list)):
+            if node_list[i].number > 0:
+                if i != 0:
+                    if node_list[i].priority_completion == True:
+                        node_list[i].more_p_stat.node += (time.next - time.current) * node_list[i].more_p_stat.number
+                        node_list[i].more_p_stat.queue += (time.next - time.current) * (node_list[i].more_p_stat.number - 1)
+                        node_list[i].more_p_stat.service += (time.next - time.current)
+
+                        node_list[i].less_p_stat.node += (time.next - time.current) * (node_list[i].less_p_stat.number - 1)
+                        node_list[i].less_p_stat.queue += (time.next - time.current) * (node_list[i].less_p_stat.number - 1)
+                        node_list[i].less_p_stat.service += (time.next - time.current)
+                    else:
+                        node_list[i].more_p_stat.node += (time.next - time.current) * (node_list[i].more_p_stat.number - 1)
+                        node_list[i].more_p_stat.queue += (time.next - time.current) * (node_list[i].more_p_stat.number - 1)
+                        node_list[i].more_p_stat.service += (time.next - time.current)
+
+                        node_list[i].less_p_stat.node += (time.next - time.current) * (node_list[i].less_p_stat.number)
+                        node_list[i].less_p_stat.queue += (time.next - time.current) * (node_list[i].less_p_stat.number - 1)
+                        node_list[i].less_p_stat.service += (time.next - time.current)
+                else:
+                    node_list[i].stat.node += (time.next - time.current) * node_list[i].number
+                    node_list[i].stat.queue += (time.next - time.current) * (node_list[i].number - 1)
+                    node_list[i].stat.service += (time.next - time.current)
+
+        current_for_update = time.current
+        time.current = time.next  # advance the clock
+
+        if time.current == node_to_process.arrival:
+            # TODO: Se sto finto switch lo metto fuori le stastiche cambiano..bah
+            # Set arrival time
+            day = (time.current / 1440.0) // 1
+            current_lambda = time.current - day * 1440.0
+
+            if 480.0 <= current_lambda < 720.0:  # 8-12
+                set_arrival_time(arrival_time_morning)
+            elif 720.0 <= current_lambda < 1020.0:  # 12-17
+                set_arrival_time(arrival_time_afternoon)
+            elif 1020.0 <= current_lambda < 1320.0:  # 17-22
+                set_arrival_time(arrival_time_evening)
+            else:  # 22-8
+                set_arrival_time(arrival_time_night)
+            if node_to_process.priority_arrival == True:  # vediamo su quale coda è stato schedulato l'arrivo che stiamo processando
+                node_to_process.more_p_stat.number += 1
+            else:
+                node_to_process.less_p_stat.number += 1
+            node_list[0].number += 1  # update system stat
+            arrival += get_arrival(arrival_time)
+            node_selected_pos = select_node(False)
+            select_queue(node_selected_pos)  # discriminazione della coda relativa all'arrivo
+            # Se il prossimo arrivo è su un altro centro, bisogna eliminare l'arrivo sul centro processato altrimenti
+            # sarà sempre il minimo
+            if node_selected_pos != node_to_process.id:
+                node_to_process.arrival = INFINITY
+            node = node_list[node_selected_pos]
+
+            if node.arrival != INFINITY:
+                node.last = node.arrival
+                if node.last is not None and node_list[0].last is not None and node_list[0].last < node.last:
+                    node_list[0].last = node.last
+            # update node and system last arrival time
+
+            # Controllo che l'arrivo sul nodo i-esimo sia valido. In caso negativo
+            # imposto come ultimo arrivo del nodo i-esimo l'arrivo precedentemente
+            # considerato
+            if arrival > STOP:
+                if node.arrival != INFINITY:
+                    node.last = node.arrival
+                # update node and system last arrival time
+                if node_list[0].last < node.last:
+                    node_list[0].last = node.last
+                node.arrival = INFINITY
+            else:
+                node.arrival = arrival
+
+            if node_to_process.more_p_stat.number == 1 and node_to_process.less_p_stat.number == 0:  # TODO: se la seconda condizione non è soddisfatta, il servizio dell'unico job della coda prioritaria non verrà schedulato ?
+                node_to_process.completion = time.current + get_service(node_to_process.id)
+                node_to_process.priority_completion = True
+            elif node_to_process.more_p_stat.number == 0 and node_to_process.less_p_stat.number == 1:
+                node_to_process.completion = time.current + get_service(node_to_process.id)  # TODO:cambiare get service che dipende da priority_completion (impostare il tasso di servizion giusto in caso di ticket queue)
+                node_to_process.priority_completion = False  # TODO: perchè per quanto riguarda le code arcade il tasso di servizio è comunque lo stesso per premium e standard ticket
+        else:
+            node_to_process.index += 1  # node stats update
+            node_to_process.number -= 1
+            if node_to_process.id != TICKET_QUEUE:  # system stats update
+                node_list[0].index += 1
+                node_list[0].number -= 1
+
+            if node_to_process.number > 0:
+                node_to_process.completion = time.current + get_service(node_to_process.id)
+            else:
+                node_to_process.completion = INFINITY
+
+            if node_to_process.id == TICKET_QUEUE:  # a completion on TICKET_QUEUE trigger an arrival on ARCADE_i
+                arcade_node = node_list[select_node(True)]  # on first global stats
+
+                # Update partial stats for arcade nodes
+                # if arcade_node.number > 0:
+                #     arcade_node.stat.node += (time.next - current_for_update) * arcade_node.number
+                #     arcade_node.stat.queue += (time.next - current_for_update) * (arcade_node.number - 1)
+                #     arcade_node.stat.service += (time.next - current_for_update)
+
+                arcade_node.number += 1  # system stats don't updated
+                arcade_node.last = time.current
+
+                if arcade_node.number == 1:
+                    arcade_node.completion = time.current + get_service(arcade_node.id)
+
+        arrival_list = [node_list[n].arrival for n in range(1, len(node_list))]
+        min_arrival = sorted(arrival_list, key=lambda x: (x is None, x))[0]
+
+    for i in range(0, len(node_list)):
+        print(node_list[i].last)
+        print("\n\nNode " + str(i))
+        print("\nfor {0} jobs".format(node_list[i].index))
+        print("   average interarrival time = {0:6.6f}".format(node_list[i].last / node_list[i].index))
+        print("   average wait ............ = {0:6.6f}".format(node_list[i].stat.node / node_list[i].index))
+        print("   average delay ........... = {0:6.6f}".format(node_list[i].stat.queue / node_list[i].index))
+        print("   average # in the node ... = {0:6.6f}".format(node_list[i].stat.node / time.current))
+        print("   average # in the queue .. = {0:6.6f}".format(node_list[i].stat.queue / time.current))
+        print("   utilization ............. = {0:6.6f}".format(node_list[i].stat.service / time.current))
